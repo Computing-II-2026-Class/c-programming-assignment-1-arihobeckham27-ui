2ec000f5cc4c383e8ff82cdc32d28dac8c519d4d
arihobeckham27-ui <arihobeckham27@gmail.com>
2026-03-24 20:37:52 +0300

    Update ex1_mobile_money.c

--- ex1_mobile_money.c
+++ ex1_mobile_money.c
@@ -1,11 +1,17 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
 
-#include <stdio.h>
+//ARIHO BECKHAM
+//25/U/BIE/01360/PE
+#include<stdio.h>
+int main(){
+    float amount_to_send, total_deducted, fee_amount, fee_percentage;
+    fee_percentage=1.5;
 
-int main(void)
-{
-    /* Write your code here */
-
+    printf("enter amount to send");
+    scanf("%f",&amount_to_send);
+    
+    fee_amount=(fee_percentage/100)*amount_to_send;
+    total_deducted=amount_to_send+fee_amount;
+    printf("fee_amount:%.2f\n",&fee_amount);
+    printf("total_amount_deducted:%.2f\n",&total_deducted);
     return 0;
 }
